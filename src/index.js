c8b0e1a0dd4816bb3c69dd89ad1ecb8a3d014c73
Julian Francis <jgfrancis3@gmail.com>
2026-05-13 10:41:52 -0400

    Task list with extra priority

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector('#create-task-form');
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const input = document.querySelector('#new-task-description');
+    const prioritySelect = document.querySelector('#task-priority');
+    
+    const taskText = input.value;
+    const priorityValue = prioritySelect.value;
+
+    const li = document.createElement('li');
+    li.textContent = `${taskText} `; 
+
+    if (priorityValue === 'high') {
+      li.style.color = 'red';
+    } else if (priorityValue === 'medium') {
+      li.style.color = 'orange'; 
+    } else if (priorityValue === 'low') {
+      li.style.color = 'green';
+    }
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.style.marginLeft = '10px';
+
+    deleteBtn.addEventListener('click', () => {
+      li.remove(); 
+    });
+
+    li.appendChild(deleteBtn);
+    const taskList = document.querySelector('#tasks');
+    taskList.appendChild(li);
+  });
 });
+
+
